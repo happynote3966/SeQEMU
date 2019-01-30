f47555a4a03703babbd22b128f5906c9ef01b9df
happynote3966 <01ssrmikururudevice01@gmail.com>
2019-01-30 09:18:00 +0900

    update demo binary

--- build_i386_linux_user/dtc/version_gen.h
+++
@@ -1,1 +0,0 @@
-#define DTC_VERSION "DTC 1.4.6-g810d5657"
--- build_i386_linux_user/test-nx.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-char shellcode[] = "\x68\x2f\x73\x68\x00\x68\x2f\x62\x69\x6e\x89\xe3\x31\xd2\x52\x53\x89\xe1\xb8\x0b\x00\x00\x00\xcd\x80";
-
-int main()
-{
-    printf("Now, let's start shellcode!\n");
-    (*(void (*)())shellcode)();
-}
-
